d606902a679efc41bb5e2edabf5c23a29cc2d5b4
Miko <jherjati@gmail.com>
2023-03-26 16:19:17 +0700

    make about us page dynamic

--- pages/about-us.js
+++ pages/about-us.js
@@ -2,9 +2,64 @@ import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import AboutUs from "../components/AboutUs";
-import { useState } from "react";
+import { request, gql } from "graphql-request";
 
-export default function Home() {
+export async function getStaticProps() {
+  const query = gql`
+    {
+      items: aboutPages(first: 1) {
+        nodes {
+          ...AboutPageFields
+        }
+      }
+      adds: homePages(first: 1) {
+        nodes {
+          ...HomePageFields
+        }
+      }
+    }
+
+    fragment AboutPageFields on AboutPage {
+      title
+      description
+      subdescription
+      section1Title
+      section1Description
+      section2Title
+      section2Description
+      section4Title
+      section4Description
+      section4Images {
+        mediaItemId
+        mediaItemUrl
+        title
+        altText
+        caption
+        description
+      }
+    }
+
+    fragment HomePageFields on HomePage {
+      section5Title
+      section5Description
+      section6Text
+    }
+  `;
+
+  const { items, adds } = await request(
+    "https://www.handalselaras.com/graphql/",
+    query
+  );
+
+  return {
+    props: {
+      data: { ...items.nodes[0], ...adds.nodes[0] },
+    },
+    revalidate: 1 * 30,
+  };
+}
+
+export default function Home({ data }) {
   return (
     <div>
       <Head>
@@ -13,7 +68,7 @@ export default function Home() {
         <link rel='icon' href='/logo.ico' />
       </Head>
       <Navbar />
-      <AboutUs />
+      <AboutUs pageData={data} />
       <Footer />
     </div>
   );
