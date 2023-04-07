3a3232e85467e6bcb9bddd9e3fb5ceb663eda887
Miko <jherjati@gmail.com>
2023-04-07 11:41:44 +0700

    make contact us page dynamic

--- components/ContactUs.jsx
+++ components/ContactUs.jsx
@@ -8,34 +8,52 @@ import accentRedBlueHalf from "../public/accents/ac-red-blue-half.svg";
 import accentGreen from "../public/accents/ac-home-green.svg";
 import Image from "next/image";
 
-const ContactUs = () => {
+const ContactUs = ({ pageData }) => {
+  const { title, address, email, phone, whatsapp } = pageData;
+
   return (
     <div className='pt-[70px] pb-[90px] flex flex-col px-5  md:px-[120px] relative'>
-      <Image src={accentGreen} alt='' className='md:block hidden absolute top-0 left-0' />
-      <Image src={accentRedBlue} alt='' className='md:block hidden absolute top-1/4 right-0' />
-      <Image src={accentRedBlueHalf} alt='' className='md:hidden absolute top-1/4 right-0' />
+      <Image
+        src={accentGreen}
+        alt=''
+        className='md:block hidden absolute top-0 left-0'
+      />
+      <Image
+        src={accentRedBlue}
+        alt=''
+        className='md:block hidden absolute top-1/4 right-0'
+      />
+      <Image
+        src={accentRedBlueHalf}
+        alt=''
+        className='md:hidden absolute top-1/4 right-0'
+      />
       <div className=' flex flex-col pb-[60px] space-y-8 md:grid md:grid-cols-3  content-center pt-10  md:pt-[90px]'>
         <div className='md:col-span-2 flex justify-center flex-col space-y-2 md:space-y-8 col-span-1 mx-3'>
-          <h1 className='text-slate-900 font-bold  text-[32px] md:text-left text-center md:text-5xl font-quicksand '>Get in Touch With Us</h1>
+          <h1 className='text-slate-900 font-bold  text-[32px] md:text-left text-center md:text-5xl font-quicksand '>
+            {title}
+          </h1>
           <div className='flex flex-col gap-4 md:gap-6 pb-5'>
             <div className='flex items-start space-x-1 gap-2'>
               <HiOfficeBuilding className='text-sm md:text-2xl text-[#8F8C8C] cursor-pointer mt-1 hover:text-sky-200' />
-              <span>
-                <p className='text-[#8F8C8C] md:text-base text-sm '>Pondok Pinang Center C-34 Jalan Ciputat Raya,</p>
-                <p className='text-[#8F8C8C] md:text-base text-sm '>Kota Jakarta Selatan, DKI Jakarta 12310</p>
-              </span>
+              <p className='text-[#8F8C8C] md:text-base text-sm w-96'>
+                {address}
+              </p>
             </div>
             <div className='flex items-center space-x-1 gap-2'>
               <MdEmail className='text-sm md:text-2xl text-[#8F8C8C] cursor-pointer  hover:text-sky-200' />
-              <p className='text-[#8F8C8C] md:text-base text-sm'>info@handalselaras.com</p>
+              <p className='text-[#8F8C8C] md:text-base text-sm'>{email}</p>
             </div>
             <div className='flex items-center space-x-1 gap-2'>
               <MdCall className='text-sm md:text-2xl text-[#8F8C8C] cursor-pointer  hover:text-sky-200' />
-              <p className='text-[#8F8C8C] md:text-base text-sm'>0851-9514-5758</p>
+              <p className='text-[#8F8C8C] md:text-base text-sm'>{phone}</p>
             </div>
-            <div className='flex items-center space-x-1 gap-2 cursor-pointer' onClick={() => window.open("https://wa.me/6285195145758", "_blank")}>
+            <div
+              className='flex items-center space-x-1 gap-2 cursor-pointer'
+              onClick={() => window.open("https://wa.me/" + whatsapp, "_blank")}
+            >
               <FaWhatsapp className='text-sm md:text-2xl text-[#8F8C8C] cursor-pointer  hover:text-sky-200' />
-              <p className='text-[#8F8C8C] md:text-base text-sm'>0851-9514-5758</p>
+              <p className='text-[#8F8C8C] md:text-base text-sm'>{whatsapp}</p>
             </div>
           </div>
         </div>
@@ -87,7 +105,9 @@ const ContactUs = () => {
               placeholder='Tuliskan pesan anda'
             />
           </div>
-          <button className=' w-full col-span-2 py-2 rounded-xl bg-blue-dark  text-white font-semibold hover:bg-opacity-80 transition-opacity ease-in-out'>Kirim</button>
+          <button className=' w-full col-span-2 py-2 rounded-xl bg-blue-dark  text-white font-semibold hover:bg-opacity-80 transition-opacity ease-in-out'>
+            Kirim
+          </button>
         </div>
         <div className='md:col-span-4 h-[375px] md:h-full w-full'>
           <Map />
--- pages/contact-us.js
+++ pages/contact-us.js
@@ -2,8 +2,41 @@ import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import ContactUs from "../components/ContactUs";
+import { request, gql } from "graphql-request";
 
-export default function Contact() {
+export async function getStaticProps() {
+  const query = gql`
+    {
+      items: contactPages(first: 1) {
+        nodes {
+          ...ContactPageFields
+        }
+      }
+    }
+
+    fragment ContactPageFields on ContactPage {
+      title
+      address
+      email
+      phone
+      whatsapp
+    }
+  `;
+
+  const { items } = await request(
+    "https://www.handalselaras.com/graphql/",
+    query
+  );
+
+  return {
+    props: {
+      data: items.nodes[0],
+    },
+    revalidate: 1 * 30,
+  };
+}
+
+export default function Contact({ data }) {
   return (
     <div>
       <Head>
@@ -12,7 +45,7 @@ export default function Contact() {
         <link rel='icon' href='/logo.ico' />
       </Head>
       <Navbar />
-      <ContactUs />
+      <ContactUs pageData={data} />
       <Footer />
     </div>
   );
